fc16d09bf1ca80b098ea0ae1656ab5c98db90d19
Randall Hunt <randall.hunt@10gen.com>
2012-06-07 21:02:30 -0400

    added comments

--- eliot.py
+++ eliot.py
@@ -15,6 +15,7 @@ def home_page():
 @app.route('/vote/<string:topic>/', methods=['GET'])
 def vote(topic):
     try:
+        # Try to update, if the topic doesn't exist then ignore the failure
         mongo.db.topics.update({'name': topic}, {"$inc": {"votes": 1}})
     finally:
         return flask.redirect('/')
@@ -23,9 +24,12 @@ def vote(topic):
 @app.route('/add_topic/', methods=['POST'])
 def add_topic():
     try:
+        # if the topic isn't empty then insert it, you probably should do some
+        # kind of "cleanup" of the input here
         if flask.request.form.get('topic'):
             mongo.db.topics.insert({'name': flask.request.form.get('topic'), 'votes': 0}, {"unique": "true"})
     finally:
         return flask.redirect('/')
 
+# run the app on localhost:5000
 app.run()
